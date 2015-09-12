3e8acb74486b671bfd9b61bac682b1e702424115
Evan <evanunderscore@gmail.com>
2015-09-12 20:54:59 +1000

    Change some files

--- src/maths.lua
+++ src/maths.lua
@@ -1,3 +1,7 @@
 function adder(a, b)
 	return a + b
 end
+
+function subber(a, b)
+	return a - b
+end
--- test/test.lua
+++ test/test.lua
@@ -1,3 +1,4 @@
 require 'src/maths'
 
 print('1 + 2 = ' .. adder(1, 2))
+print('1 - 2 = ' .. subber(1, 2))
